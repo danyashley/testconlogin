2a591b80707d854c80ae158e49bd540381aea6cb
danyashley <dany.perezmosquera@gmail.com>
2018-03-14 02:51:00 -0300

    n

--- function.js
+++ function.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+	 $("#errorMsg").hide();
+    $("#btnLogin").click(function(){
+        var usu = $("#txtuser").val();
+        var pass = $("#txtpassword").val();
+        $.post("http://greenmunchies.tk/app712/login.php",{ usu : usu, pass : pass},function(respuesta){
+            if (respuesta == true) {
+                $.mobile.changePage("#home");
+					
+
+            }
+            else{
+                $.mobile.changePage('#pageError', 'pop', true, true);
+                /*$("#errorMsg").fadeIn(300);
+                $("#errorMsg").css("display", "block");*/
+            }
+        });
+    });
    
         $.getJSON("http://greenmunchies.tk/app712/traer.php", function(resultados){
             
