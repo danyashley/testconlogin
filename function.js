66080140d3c1ff55481d22ee800f9ef777320e5a
danyashley <dany.perezmosquera@gmail.com>
2018-03-14 03:49:55 -0300

    dd

--- function.js
+++ function.js
@@ -1,24 +1,4 @@
 $(document).ready(function(){
-		$('#listacom').empty();
-		$('#vermas').empty();
-
-	 $("#errorMsg").hide();
-    $("#btnLogin").click(function(){
-        var usu = $("#txtuser").val();
-        var pass = $("#txtpassword").val();
-        $.post("http://greenmunchies.tk/app712/login.php",{ usu : usu, pass : pass},function(respuesta){
-            if (respuesta == true) {
-                $.mobile.changePage("#home");
-					
-
-            }
-            else{
-                $.mobile.changePage('#pageError', 'pop', true, true);
-                /*$("#errorMsg").fadeIn(300);
-                $("#errorMsg").css("display", "block");*/
-            }
-        });
-    });
    
         $.getJSON("http://greenmunchies.tk/app712/traer.php", function(resultados){
             
@@ -47,7 +27,6 @@ $(document).ready(function(){
 		
 
     });
-	
 
 
 	function info(x){
